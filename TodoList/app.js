3d234efb0290aee6472e27ba8ede94e67ead4335
satya-subham <satyasubham85@gmail.com>
2023-10-21 19:47:04 +0530

    new functionality added

--- TodoList/app.js
+++ TodoList/app.js
@@ -2,43 +2,67 @@ let enterButton = document.getElementById("enter");
 let input = document.getElementById("userInput");
 let ul = document.querySelector("ul");
 
-
-function createListElement(){
-    let li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-    input.value = "";
-    function crossOut(){
-        li.classList.toggle("done");
+function createListElement() {
+  let listElements = document.getElementsByTagName("li");
+  for (let list of listElements) {
+    const innertext = list.innerText;
+    const todo = innertext.substring(0, innertext.indexOf("\n"));
+    if (todo.toLowerCase() === input.value.toLowerCase()) {
+      alert("Same todo found", input.value);
+      input.value = "";
+      return;
     }
-    function strikeOut(){
-        li.style.textDecoration = "line-through";
-        li.removeEventListener("click", crossOut);
-    }
-    li.addEventListener("click", crossOut);
-    li.addEventListener("dblclick", strikeOut);
+  }
 
+  let li = document.createElement("li");
+  let liText = document.createTextNode(input.value);
+  li.appendChild(liText);
+  ul.appendChild(li);
+  input.value = "";
+  function crossOut() {
+    li.classList.toggle("done");
+  }
+  function strikeOut() {
+    let completeTodoUl = document.querySelector(".complete-todo");
+    let completeTodoLi = document.createElement("li");
+    let completeTododLiText = liText;
+    completeTodoLi.appendChild(completeTododLiText);
+    completeTodoUl.appendChild(completeTodoLi);
     let deleteButton = document.createElement("button");
     deleteButton.appendChild(document.createTextNode("X"));
-    li.appendChild(deleteButton);
+    completeTodoLi.appendChild(deleteButton);
+    completeTodoLi.style.textDecoration = "line-through";
+    completeTodoLi.style.backgroundColor = "gray";
+    deleteListItem();
+    li.removeEventListener("click", crossOut);
+    deleteButton.addEventListener("click", ()=>{
+        completeTodoLi.classList.toggle("delete");
+    });
+  }
+  li.addEventListener("click", crossOut);
+  li.addEventListener("dblclick", strikeOut);
 
-    deleteButton.addEventListener("click", deleteListItem);
+  let deleteButton = document.createElement("button");
+  deleteButton.appendChild(document.createTextNode("X"));
+  li.appendChild(deleteButton);
 
-    function deleteListItem(){
-        li.classList.toggle("delete");
-        // li.remove();
-    }
+  deleteButton.addEventListener("click", deleteListItem);
+
+  function deleteListItem() {
+    li.classList.toggle("delete");
+    // li.remove();
+  }
 }
 
-enterButton.addEventListener("click", ()=>{
-    if(input.value){
-        createListElement();
-    }
+enterButton.addEventListener("click", () => {
+  if (input.value) {
+    createListElement();
+  }
 });
 
-function addListAfterKeyPress(event){
-    if(input.value && event.key === "Enter"){
-        createListElement();
-    }
+function addListAfterKeyPress(event) {
+  if (input.value && event.key === "Enter") {
+    createListElement();
+  }
 }
-input.addEventListener("keypress", addListAfterKeyPress)
+input.addEventListener("keypress", addListAfterKeyPress);
